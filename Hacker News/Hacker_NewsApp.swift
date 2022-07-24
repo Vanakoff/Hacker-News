c6fb87ce043c24d6ead0833a1048b1cc3b736045
Vanakoff <andrey.vanakoff@gmail.com>
2022-07-24 16:13:15 +0200

    Initial Commit

---
+++ Hacker News/Hacker_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hacker_NewsApp.swift
+//  Hacker News
+//
+//  Created by Andrey Vanakoff on 24/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct Hacker_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
