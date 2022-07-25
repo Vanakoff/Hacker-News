8c992ff42c9b66b2c080282486ac0e704436009f
Vanakoff <andrey.vanakoff@gmail.com>
2022-07-26 00:34:30 +0200

    Detail View created. UIkit WebView implemented.

---
+++ Hacker News/Views/DetailView.swift
@@ -0,0 +1,24 @@
+//
+//  DetailView.swift
+//  Hacker News
+//
+//  Created by Andrey Vanakoff on 25/07/2022.
+//
+
+import SwiftUI
+import WebKit
+
+struct DetailView: View {
+    let url: String?
+    
+    var body: some View {
+        WebView(urlString: url)
+    }
+}
+
+struct DetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailView(url: "https://www.google.com")
+    }
+}
+
---
+++ Hacker News/Views/WebView.swift
@@ -0,0 +1,28 @@
+//
+//  WebView.swift
+//  Hacker News
+//
+//  Created by Andrey Vanakoff on 26/07/2022.
+//
+
+import Foundation
+import WebKit
+import SwiftUI
+
+struct WebView: UIViewRepresentable {
+
+    let urlString: String?
+   
+    func makeUIView(context: Context) -> WKWebView {
+        return WKWebView()
+    }
+    
+    func updateUIView(_ uiView: WKWebView, context: Context) {
+        if let safeString = urlString {
+            if let url = URL(string: safeString) {
+                let request = URLRequest(url: url)
+                uiView.load(request)
+            }
+        }
+    }
+}
